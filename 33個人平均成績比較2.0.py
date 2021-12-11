410ce0a6190bdaf4c2cb5174fba705fb363d136c
Lo'N <84562177+LoN-1230@users.noreply.github.com>
2021-12-11 16:26:39 +0800

    Semester-average-calculator

---
+++ 33個人平均成績比較2.0.py
@@ -0,0 +1,21 @@
+import sys
+x = float(input("請輸入國文成績"))
+y = float(input("請輸入數學成績"))
+z = float(input("請輸入英文成績"))
+grade = (x + y + z) / 3
+if x < 1 or x > 100:
+    print("國文成績錯誤")
+if y < 1 or y > 100:
+    print("數學成績錯誤")
+if z < 1 or z > 100:
+    print("英文成績錯誤")
+    sys.exit("成績應在1~100間")
+else: x or y or z >= 1 or x  or y or z <= 100
+print("學期成績平均是" + str(grade))    
+if grade < 70:
+    print("小於全班平均")
+elif grade == 70:
+    print("等於全班平均")
+else:
+    print("高於全班平均")
+    
