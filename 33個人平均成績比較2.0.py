eb490be55f42664d02e383f6b6a5bef6eb7022fc
Lo'N <84562177+LoN-1230@users.noreply.github.com>
2021-12-17 19:05:59 +0800

    Semester-average-calculator

--- 33個人平均成績比較2.0.py
+++ 33個人平均成績比較2.0.py
@@ -13,7 +13,7 @@ if z < 1 or z > 100:
     print("英文成績錯誤")
     sys.exit("成績應在1~100間")
 else:
-  print("學期成績平均是" + str(grade))    
+    print("學期成績平均是" + str(grade))    
 if grade < 70:
     print("小於全班平均")
 elif grade == 70:
